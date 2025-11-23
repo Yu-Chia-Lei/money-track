6e662c44a06e5ceeba3cc48034daf0d7cd5d07c2
Joyce <s411278013@gm.ntpu.edu.tw>
2025-11-23 11:56:28 +0800

    嘗試部署到Zeaber

---
+++ apps/core/templatetags/custom_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def lookup(list_of_social_accounts, provider_name):
+    """
+    過濾器：從社交帳號列表中找出指定 provider 的帳號。
+    用法: {% with google_account=accounts|lookup:"google" %}
+    """
+    # list_of_social_accounts 是一個 SocialAccount QuerySet
+    # provider_name 例如 'google', 'facebook'
+    try:
+        return list_of_social_accounts.filter(provider=provider_name)
+    except AttributeError:
+        return []
